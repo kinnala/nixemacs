2f1dd2d339db3fcf519e68ccadfa0d0188bf447f
Tom Gustafsson <tom.gustafsson@iki.fi>
2019-12-26 18:35:26 +0200

    Don't install browser

--- overlays/tom.nix
+++ overlays/tom.nix
@@ -3,7 +3,6 @@ self: super: {
     emacs = self.emacs;
     nix = self.nix;
     conda = self.conda;
-    google-chrome = self.google-chrome;
     slack = self.slack;
     gnupg = self.gnupg;
     pass = self.pass;
